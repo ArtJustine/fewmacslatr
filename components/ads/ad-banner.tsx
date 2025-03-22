8ec77e626f956f5c2764e435be896dd815a62fa7
ArtJustine <152577317+ArtJustine@users.noreply.github.com>
2025-03-22 19:00:30 +0800

    Update ad-banner.tsx

--- components/ads/ad-banner.tsx
+++ components/ads/ad-banner.tsx
@@ -13,9 +13,10 @@ export function AdBanner({ size, className = "", id }: AdBannerProps) {
   return (
     <div
       id={id}
-      className={`bg-gray-100 border border-gray-200 flex items-center justify-center mx-auto ${className}`}
+      className={`bg-gray-100 border border-gray-200 flex flex-col items-center justify-center mx-auto ${className}`}
       style={{ width, height, maxWidth: "100%" }}
     >
+      {/* Iframe Ad */}
       <iframe
         id="iframe_342"
         src="//a.impactradius-go.com/gen-ad-code/2406390/2840060/18023/"
@@ -26,6 +27,29 @@ export function AdBanner({ size, className = "", id }: AdBannerProps) {
         marginHeight={0}
         marginWidth={0}
       ></iframe>
+
+      {/* Banner Ad */}
+      <a
+        rel="sponsored"
+        href="https://belkin.evyy.net/c/2406390/276459/3736"
+        target="_top"
+        id="276459"
+      >
+        <img
+          src="//a.impactradius-go.com/display-ad/3736-276459"
+          border="0"
+          alt=""
+          width={width}
+          height={height}
+        />
+      </a>
+      <img
+        height="0"
+        width="0"
+        src="https://belkin.evyy.net/i/2406390/276459/3736"
+        style={{ position: "absolute", visibility: "hidden" }}
+        border="0"
+      />
     </div>
   );
 }
