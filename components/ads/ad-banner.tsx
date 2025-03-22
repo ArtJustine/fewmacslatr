4b57e1951f14ed8eab7c65cd4c94dc2c2da6daf6
ArtJustine <152577317+ArtJustine@users.noreply.github.com>
2025-03-22 19:02:59 +0800

    Update ad-banner.tsx

--- components/ads/ad-banner.tsx
+++ components/ads/ad-banner.tsx
@@ -13,43 +13,57 @@ export function AdBanner({ size, className = "", id }: AdBannerProps) {
   return (
     <div
       id={id}
-      className={`bg-gray-100 border border-gray-200 flex flex-col items-center justify-center mx-auto ${className}`}
+      className={`bg-gray-100 border border-gray-200 flex items-center justify-center mx-auto ${className}`}
       style={{ width, height, maxWidth: "100%" }}
     >
-      {/* Iframe Ad */}
-      <iframe
-        id="iframe_342"
-        src="//a.impactradius-go.com/gen-ad-code/2406390/2840060/18023/"
-        width={width}
-        height={height}
-        scrolling="no"
-        frameBorder="0"
-        marginHeight={0}
-        marginWidth={0}
-      ></iframe>
-
-      {/* Banner Ad */}
-      <a
-        rel="sponsored"
-        href="https://belkin.evyy.net/c/2406390/276459/3736"
-        target="_top"
-        id="276459"
-      >
-        <img
-          src="//a.impactradius-go.com/display-ad/3736-276459"
-          border="0"
-          alt=""
+      {/** Show the iframe ad only for 300x250 (if applicable) */}
+      {size === "300x250" && (
+        <iframe
+          id="iframe_342"
+          src="//a.impactradius-go.com/gen-ad-code/2406390/2840060/18023/"
           width={width}
           height={height}
-        />
-      </a>
-      <img
-        height="0"
-        width="0"
-        src="https://belkin.evyy.net/i/2406390/276459/3736"
-        style={{ position: "absolute", visibility: "hidden" }}
-        border="0"
-      />
+          scrolling="no"
+          frameBorder="0"
+          marginHeight={0}
+          marginWidth={0}
+        ></iframe>
+      )}
+
+      {/** Show the image ad only for 300x250 */}
+      {size === "300x250" && (
+        <>
+          <a
+            rel="sponsored"
+            href="https://belkin.evyy.net/c/2406390/276459/3736"
+            target="_top"
+            id="276459"
+          >
+            <img
+              src="//a.impactradius-go.com/display-ad/3736-276459"
+              border="0"
+              alt="Advertisement"
+              width={width}
+              height={height}
+            />
+          </a>
+          <img
+            height="0"
+            width="0"
+            src="https://belkin.evyy.net/i/2406390/276459/3736"
+            style={{ position: "absolute", visibility: "hidden" }}
+            border="0"
+          />
+        </>
+      )}
+
+      {/** Placeholder if no matching ad is available */}
+      {!["300x250"].includes(size) && (
+        <div className="text-center text-gray-400 text-sm">
+          <p>Advertisement</p>
+          <p className="text-xs">{size}</p>
+        </div>
+      )}
     </div>
   );
 }
